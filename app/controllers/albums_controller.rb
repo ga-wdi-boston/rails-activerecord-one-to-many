2d4f021fedf91615929d641c950ba20305f3c20a
David Fisher <tibbon@gmail.com>
2014-01-29 12:24:34 -0500

    Removed create.html.erb

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,7 +6,7 @@ class AlbumsController < ApplicationController
   def create
     @album = Album.new(album_params)
     if @album.save
-      redirect_to action: :index
+      redirect_to artists_path
     else
       render :new
     end
