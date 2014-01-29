946f73a8217518d429795f210bd6c2207c47301a
David Fisher <tibbon@gmail.com>
2014-01-29 15:21:38 -0500

    Songs added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 artist_one = Artist.create(name: "Nirvana")
 
-artist_one.albums << Album.create(name: "Nevermind")
+nevermind = artist_one.albums << Album.create(name: "Nevermind")
 artist_one.albums << Album.create(name: "In Utereo")
 artist_one.albums << Album.create(name: "Unplugged")
 artist_one.albums << Album.create(name: "Bleach")
+
+nevermind.songs << Song.create(name: "Smells like Teen Spirit")
+nevermind.songs << Song.create(name: "In Bloom")
+nevermind.songs << Song.create(name: "Lithium")
