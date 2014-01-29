91e96bc646eff7af8e6e5dca1a86a63fc276569c
David Fisher <tibbon@gmail.com>
2014-01-29 11:58:58 -0500

    Ran controller Artist generator

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,10 @@
+class ArtistsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spotterfly::Application.routes.draw do
+  get "artists/show"
+  get "artists/index"
+  get "artists/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
