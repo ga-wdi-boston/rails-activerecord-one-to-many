781505b91c32ed5d0df91ba3cb36d82425fd5442
David Fisher <tibbon@gmail.com>
2014-01-30 14:17:42 -0500

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-artist_one = Artist.create(name: "Nirvana")
+# artist_one = Artist.create(name: "Nirvana")
 
-nevermind = artist_one.albums << Album.create(name: "Nevermind")
-artist_one.albums << Album.create(name: "In Utereo")
-artist_one.albums << Album.create(name: "Unplugged")
-artist_one.albums << Album.create(name: "Bleach")
+# nevermind = artist_one.albums << Album.create(name: "Nevermind")
+# artist_one.albums << Album.create(name: "In Utereo")
+# artist_one.albums << Album.create(name: "Unplugged")
+# artist_one.albums << Album.create(name: "Bleach")
 
-nevermind.songs << Song.create(name: "Smells like Teen Spirit")
-nevermind.songs << Song.create(name: "In Bloom")
-nevermind.songs << Song.create(name: "Lithium")
+# nevermind.songs << Song.create(name: "Smells like Teen Spirit")
+# nevermind.songs << Song.create(name: "In Bloom")
+# nevermind.songs << Song.create(name: "Lithium")
 
 king = Author.create(name: "Stephen King")
 austen = Author.create(name: "Jane Austen")
