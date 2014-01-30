4aa664f7aff1960d5a77aa879ad853cce2e19aaa
Alex Grant <alex@grantovich.net>
2014-01-30 10:51:27 -0500

    Allow leaving comments on articles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :find_user_and_article
+
   def new
+    @comment = @article.comments.new
   end
 
   def create
+    @article.comments << Comment.create!(comment_params)
+    redirect_to [@user, @article]
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_user_and_article
+    @user = User.find(params[:user_id])
+    @article = Article.find(params[:article_id])
   end
 end
