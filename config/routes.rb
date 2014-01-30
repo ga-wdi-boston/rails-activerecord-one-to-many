8508e34e07af7d48d69556cbed34611899548904
David Fisher <tibbon@gmail.com>
2014-01-30 14:32:03 -0500

    Nested Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Spotterfly::Application.routes.draw do
   resources :albums, only: [:create, :new]
   resources :artists, only: [:index, :show, :create]
+
+  # We can nest either way
+  # In reality, we'd probably just one want way
+  resources :authors do
+    resources :books
+  end
+
+  resources :books do
+    resources :authors
+  end
+
   root to: 'artists#index'
 end
