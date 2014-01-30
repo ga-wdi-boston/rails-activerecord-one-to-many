e76f3acaaca1f9d58c04dd8304f67f3ea8939c10
David Fisher <tibbon@gmail.com>
2014-01-30 15:04:54 -0500

    Added links to new

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,44 @@
 class AuthorsController < ApplicationController
+  before_action :get_author, only: [:show, :edit, :update, :destroy]
+
   def show
+    @author = Author.find(params[:id])
   end
 
   def index
-    @authors = Author.all
+    if params[:book_id]
+      @book = Book.find(params[:book_id])
+      @authors = @book.authors
+    else
+      @authors = Author.all
+    end
+  end
+
+  def update
+    @author.update(author_params)
+    redirect_to @author
+  end
+
+  def edit
+  end
+
+  def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new(author_params)
+    @author.save!
+    redirect_to @author
+  end
+
+  private
+
+  def get_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:name, book_ids: [])
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,43 @@
 class BooksController < ApplicationController
+  before_action :get_book, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def index
-    @books = Book.all
+    if params[:author_id]
+      @author = Author.find(params[:author_id])
+      @books = @author.books
+    else
+      @books = Book.all
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save!
+    redirect_to @book
+  end
+
+  private
+
+  def get_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:name, author_ids: [])
   end
 end
