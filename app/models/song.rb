5abe0f57cb2dce3bbc84c8ecc1c67c64686a1faf
Tom Dyer <tdyer@ourstage.com>
2014-06-30 11:23:07 -0400

    Added comments to routes and the song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
+  # defines getter, setter and some other methods 
+  # for the Song model.
   belongs_to :album
+
+  # # getter
+  # def album
+  #   @album
+  # end
+
+  # # setter
+  # def album=(album)
+  #   @album = album
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   resources :albums, only: [:index, :show]
+
+  # Set the root route .
+  # This will be the controller/action that gets 
+  # called when you enter '/'
   root 'albums#index'
 
 end
