c2e38de3c8667332840213ab53c37b22464de79e
Tom Dyer <tdyer@ourstage.com>
2014-06-30 11:23:34 -0400

    Added a has_many relationship to the album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,19 @@
 class Album < ActiveRecord::Base
+
+  has_many :songs
+  # has_many :all_album_songs, class_name: :songs
+
+  # # Get all this albums songs.
+  # def songs
+  #   @songs = Song.where(album_id: self.id)
+  #   select * from songs where album_id = 3
+  # end
+
+  # # Add a song to this album
+  # def songs<<(song)
+  #   song.album_id = self.id
+  #   @songs << song
+  #   song.save
+  # end
+
 end
