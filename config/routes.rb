bc70027062c3196fc03ad50630f6133411edb753
Tom Dyer <tdyer1@gmail.com>
2015-05-28 23:30:53 -0400

    revert back routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # Default root for '/' in this application
   root 'movies#index'
 
-  # create routes for movie resource
-  resources :movies, except: [:new, :edit]
+  get '/movies', to: 'movies#index'
+
+  # :id will identify a specific movie
+  get '/movies/:id', to: 'movies#show'
+
+  # Create a movie.
+  post '/movies', to: 'movies#create'
+
+  # Update a movie
+  patch '/movies/:id', to: 'movies#update'
+
+  # delete a movie
+  delete '/movies/:id', to: 'movies#delete'
 end
