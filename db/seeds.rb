d658b137f1587a6fa57ac9cc56c17d083990029c
Tom Dyer <tdyer1@gmail.com>
2015-05-28 23:56:43 -0400

    Populate DB with movie reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
+Review.delete_all
 Movie.delete_all
 
-Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123)
-Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154)
-Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105)
+movie = Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123)
+movie.reviews.create!(name: 'Tom', comment: 'Dark, somber')
+movie.reviews.create!(name: 'Meg', comment: 'Slow, boring')
+
+movie = Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154)
+movie.reviews.create!(name: 'Joe', comment: 'Explosions, silly')
+movie.reviews.create!(name: 'Christine', comment: 'Brilliant, fun')
+
+movie = Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105)
+movie.reviews.create!(name: 'Tom', comment: 'Crazy, humor')
+movie.reviews.create!(name: 'Joanne', comment: 'Waste of time, stupid')
+
 puts "Created three Movies"
