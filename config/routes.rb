e31a46df1d79fc74f8a1a43d1145b5834723711c
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-27 14:24:25 -0400

    Reset lesson
    The parent commit is archived and still accessible at 007/master.
    
    This reset focuses on continuing previous examples of relationships
    using entities that were developed first in SQL lessons, then included
    in ActiveRecord lessons.
    
    This new lesson is smaller and will focus on direct analogies between
    PORO, SQL, ActiveRecord relationships, what the relationship macros define
    as methods, and the workflow for creating new associations.

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # Default root for '/' in this application
-  root 'movies#index'
-
-  # create routes for movie resource
-  resources :movies, except: [:new, :edit]
-end
--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-Movie.delete_all
-
-Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123)
-Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154)
-Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105)
-puts "Created three Movies"
