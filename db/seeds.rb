d447a1a107e5abf607351d92571e03117a607e6f
berziiii <brian@offpistedesigns.com>
2016-06-20 10:05:21 -0400

    Add require csv to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
+
 City.transaction do
   CSV.foreach 'data/cities.csv', headers: true do |city|
     City.create(city.to_hash)
